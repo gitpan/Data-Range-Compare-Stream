df6200acc8b2c1837742db3d517e6966c623f60d
Michael Shipper <akalinux@cpan.org>
2012-03-24 16:49:44 +0000

    Import of AKALINUX/Data-Range-Compare-Stream-2.011 from CPAN.
    gitpan-cpan-distribution: Data-Range-Compare-Stream
    gitpan-cpan-version:      2.011
    gitpan-cpan-path:         AKALINUX/Data-Range-Compare-Stream-2.011.tar.gz
    gitpan-cpan-author:       AKALINUX
    gitpan-cpan-maturity:     released

--- lib/Data/Range/Compare/Stream.pm
+++ lib/Data/Range/Compare/Stream.pm
@@ -7,7 +7,7 @@ use Data::Range::Compare::Stream::Constants qw(RANGE_START RANGE_END RANGE_DATA)
 
 use constant NEW_FROM_CLASS=>'Data::Range::Compare::Stream';
 
-our $VERSION='2.010';
+our $VERSION='2.011';
 
 sub new {
   my ($class,@args)=@_;
